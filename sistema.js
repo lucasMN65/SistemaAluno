d40ebad980c1299c3773e32b0bacd0621af892fb
Lucas Motta <lucas99motta@hotmail.com>
2021-12-16 20:35:07 -0300

    sistemaAluno

---
+++ sistema.js
@@ -0,0 +1,31 @@
+let nomeDeAluno = ["Lunna", "Fabio", "Jose", "Ricardo"]
+
+for (let nome of nomeDeAluno) {
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+
+// while(contador <= numeroDeAlunos){
+//     //console.log(contador)
+//     if(contador == 0) {
+//         console.log("O número atual é zero!")
+//      }else if(contador % 2 == 0){
+//          console.log(`O número ${contador} é par!`)
+//      }else{
+//           console.log(`O número ${contador} é impar!`)
+//      }
+//      contador ++;
+//   }
+// }
+
+
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//      //console.log(contador)  
+//    if(contador == 0) {
+//        console.log("O número atual é zero!")
+//     }else if(contador % 2 == 0){
+//         console.log(`O número ${contador} é par!`)
+//     }else{
+//          console.log(`O número ${contador} é impar!`)
+//     }
+//  }
